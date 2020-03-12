fd997248ec78940ceb1adf90456ac50bb913893b
Rayapareddy <rayapa142000@gmail.com>
2020-03-12 15:41:36 +0530

    Updates

---
+++ precache-manifest.abd6e9de3cd05a257a50dc1cdb412fe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe11e63d4082c5dfdd059df6bb9d18a",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "a4364479e0f793bbfee7",
+    "url": "/project1/static/css/main.6ab6d26d.chunk.css"
+  },
+  {
+    "revision": "6357e287429b5cb2f37a",
+    "url": "/project1/static/js/2.8411d881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.8411d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4364479e0f793bbfee7",
+    "url": "/project1/static/js/main.d967d712.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "b1836e3850cb082d6722d85ddbd4b8f2",
+    "url": "/project1/static/media/super.b1836e38.svg"
+  }
+]);
